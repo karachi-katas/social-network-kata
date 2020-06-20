789e3addf6b0d8e3214e0fe06c106b4e91d7a605
hmaroof <F3hmaroof@agribeef.com>
2020-06-20 17:31:01 +0500

    tdd cycle user should be able to view posts of all followings

--- src/test/java/UserShould.java
+++ src/test/java/UserShould.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -49,6 +50,25 @@ public class UserShould {
         List<Post> posts = bob.getPostsOfFollowing();
 
         Assert.assertEquals(post, posts.get(0));
+    }
+    @Test
+    public void beAbleToViewPostsOfAllPeopleTheyAreFollowing() {
+        User alice = new User("Alice");
+        User bob   = new User("Bob");
+        User charlie = new User ("Charlie") ;
+
+        charlie.follow(alice);
+        charlie.follow(bob);
+
+        Post postByAlice = new Post("Post content from Alice", new Date(1592650942000L));
+        Post postByBob = new Post("Post content from Bob", new Date(1592650952000L));
+
+        alice.publish(postByAlice);
+        bob.publish(postByBob);
+
+        List<Post> posts = charlie.getPostsOfFollowing();
+        List<Post> expected = Arrays.asList(postByAlice,postByBob);
+        Assert.assertEquals( expected , posts );
     }
 }
 
